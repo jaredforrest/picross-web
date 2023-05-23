8b539dfeef9b56991c2f2771f5961ed2d523aad8
Jared Forrest <jared@jaredforrest.xyz>
2023-05-23 20:29:13 +0100

    Streamline build process
    Use npm for the build rather than a bash script

--- js-proj/make.sh
+++ js-proj/make.sh
@@ -2,60 +2,29 @@
 
 FLASK_DIR="../flask/project"
 
-SED='s/submit-button/b/g;
-     s/grid-cell/c/g;
-     s/empty/e/g;
-     s/filled/f/g;
-     s/grid-container/g/g;
-     s/level-name/l/g;
-     s/nono-grid/n/g;
-     s/side-nums/s/g;
-     s/top-nums/t/g;
-     s/error-msg/v/g;'
-
-# build
-npm install
+# make dist just in case
 mkdir -p dist
-npx spack &&
-npx terser --config-file terser.config.json -- ./dist/init.js > ./dist/index.js &&
-#cp ./dist/init.js ./dist/index.js &&
 
-# extra minify
-#sed -i "$SED" dist/index.js
-
-for file in template/*
-do
-#    sed  "$SED" "$file" > "dist/$(basename "$file")"
-    cp "$file" "dist/$(basename "$file")"
-done
+npm run build
 
 # copy to server
 mkdir -p $FLASK_DIR/static/css
-mkdir -p $FLASK_DIR/app/static/js
+mkdir -p $FLASK_DIR/static/js
 cp dist/index.js $FLASK_DIR/static/js/index.js
-cp dist/style.css $FLASK_DIR/static/css/style.css
+cp template/style.css $FLASK_DIR/static/css/style.css
 
-mkdir -p ../flask/project/puzzles/templates
-cp dist/*.html ../flask/project/puzzles/templates/
-
-# extra
-npx brotli-cli compress dist/index.js
+mkdir -p $FLASK_DIR/puzzles/templates
+cp template/*.html $FLASK_DIR/puzzles/templates/
 
 #make compat files
-mkdir -p ../babelify/templates
-cp dist/style.css ../babelify/templates
+mkdir -p ../babelify/template
+cp template/style.css ../babelify/template
 cat ../babelify/src/init.js dist/index.js > ../babelify/src/index.js
 
 cd ../babelify || exit
 
-npm install
 mkdir -p dist
-npx webpack
-npx terser --config-file terser.config.json -- ./dist/init.js > ./dist/index.js
-#cp ./dist/init.js ./dist/index.js
-
-# now the css
-npx postcss --use postcss-css-variables -o dist/style.css templates/style.css
+npm run build
 
 # copy to server
 mkdir -p $FLASK_DIR/static/oldjs
