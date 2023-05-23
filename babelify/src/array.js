ee8e1c6280efa2276ac71be085c16f036fdd71b5
Jared Forrest <jared@jaredforrest.xyz>
2023-05-23 10:41:54 +0100

    Update arrray polyfill

--- babelify/src/array.js
+++ babelify/src/array.js
@@ -2,10 +2,11 @@ export default function arrayPolyfill(){
  /**
   *
   * @template T
+  * @this {T[]}
   * @param {(value: T, index: number, array: T[]) => void} callbackFn 
   * @returns {void}
   */
-Array.prototype.forEach = function(callbackFn) {
+Array.prototype.forEach = Array.prototype.forEach || function(callbackFn) {
     for(let i = 0; i < this.length; i++){
         callbackFn(this[i], i, this)
     }
@@ -14,11 +15,12 @@ Array.prototype.forEach = function(callbackFn) {
  /**
   *
   * @template T, U
+  * @this {T[]}
   * @param {ArrayLike<T>} arrayLike 
   * @param {(v: T, k: number) => U} mapFn 
   * @returns {U[]}
   */
-Array.from = function(arrayLike, mapFn) {
+Array.from = Array.from || function(arrayLike, mapFn) {
     const ret = []
     for(let i = 0; i < arrayLike.length; i++){
         ret.push(mapFn(arrayLike[i],i))
@@ -29,21 +31,23 @@ Array.from = function(arrayLike, mapFn) {
  /**
   *
   * @template T, U
+  * @this {T[]}
   * @param {(t: T) => U} callbackFn 
   * @returns {U[]}
   */
-Array.prototype.map = function(callbackFn) {
+Array.prototype.map = Array.prototype.map ||  function(callbackFn) {
     return Array.from(this, callbackFn)
 }
 
  /**
   *
   * @template T, U
+  * @this {T[]}
   * @param {(accumulator: U, currentValue: T) => U} callbackFn 
-  * @param {U} initialValue 
+  * @param {U} [initialValue] 
   * @returns {U}
   */
-Array.prototype.reduce = function(callbackFn, initialValue) {
+Array.prototype.reduce = Array.prototype.reduce || function(callbackFn, initialValue) {
     let acc = (initialValue === undefined) ? this[0] : initialValue;
     const start = (initialValue === undefined) ? 1 : 0
     for (let i = start; i < this.length; i++) {
@@ -55,10 +59,11 @@ Array.prototype.reduce = function(callbackFn, initialValue) {
  /**
   *
   * @template T
+  * @this {T[]}
   * @param {(currentValue: T) => boolean} callbackFn 
   * @returns {T[]}
   */
-Array.prototype.filter = function(callbackFn) {
+Array.prototype.filter = Array.prototype.filter || function(callbackFn) {
     const ret = []
     for(let i = 0; i < this.length; i++){
         if(callbackFn(this[i])){
@@ -71,10 +76,11 @@ Array.prototype.filter = function(callbackFn) {
  /**
   *
   * @template T
+  * @this {T[]}
   * @param {T} searchElement
   * @returns {number}
   */
-Array.prototype.indexOf = function (searchElement) {
+Array.prototype.indexOf = Array.prototype.indexOf || function (searchElement) {
   for (let i = 0; i < this.length; i++) {
     if (i in this && this[i] === searchElement) {
       return i;
